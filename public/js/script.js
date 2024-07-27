9722fea3458d9bc39f3f9676cd4340386c9f9b3c
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-07-27 12:27:17 +0530

    ➕ add main homepage

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,24 @@
+const loader = document.getElementById('loader')
+
+window.addEventListener('load', () => {
+  loader.style.display = 'none';
+})
+
 const copyrightYear = document.querySelector('#copyright-year');
 const d = new Date();
 copyrightYear.innerText = d.getFullYear();
+
+
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const hamburgerBtn = document.querySelector('#hamburger-btn');
+const hamburgerBtnClose = document.querySelector('#hamburger-btn-close');
+
+
+hamburgerBtn.addEventListener('click', () => {
+    hamburgerMenu.style.transform = 'translateY(0)';
+})
+
+hamburgerBtnClose.addEventListener('click', () => {
+    hamburgerMenu.style.transform = 'translateY(-500px)';
+})
 
